735189b1218f6a5c25d88cf81b0b3f31287cec8b
Yurick <yurick.hauschild@gmail.com>
2019-11-10 02:27:08 +0100

    ♻️  Add semantic releases

---
+++ release.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    'semantic-release-gitmoji',
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          { path: 'lib/clifford.js', label: 'Standard distribution' },
+          { path: 'lib/clifford.mjs', label: 'ES6 modules distribution' },
+          { path: 'lib/clifford.modern.js', label: 'Modern JS distribution' },
+          { path: 'lib/clifford.umd.js', label: 'UMD distribution' },
+        ],
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message: '🔖 ${nextRelease.version}\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+}
